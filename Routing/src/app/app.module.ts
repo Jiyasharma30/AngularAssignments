54fd52aa31f87db28b649b49a484ab3a3f4c1426
Jiyasharma30 <57463689+Jiyasharma30@users.noreply.github.com>
2022-06-21 15:47:47 +0530

    Add files via upload

---
+++ Routing/src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { InventoryComponent } from './inventory/inventory.component';
+
+const appRoutes:Routes=[
+  {path:'Product', component:ProductComponent},
+  {path:'Inventory', component:InventoryComponent},
+  {path:'', component:ProductComponent}
+]
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductComponent,
+    InventoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
